2bc6a751ea8193c7e3120dc974771b34b23cd268
L Barbee <bluelb84@gmail.com>
2017-08-06 18:54:19 -0400

    added styling and set up app.js function stubs with user stories considered

---
+++ app.js
@@ -0,0 +1,29 @@
+function renderShoppingList() {
+    // this function will be responsible for rendering the shopping lise in
+    // the DOM
+    console.log('`renderShoppingList` ran');
+}
+
+function handleNewItemSubmit() {
+    // this function will be responsible for when users add a new shopping list item
+    console.log('`handleNewItemSubmit` ran');
+}
+
+function handleItemCheckClicked() {
+    // this function will be responsible for when users click the "check" button on
+    // a shopping list item.
+    console.log('`handleItemCheckClicked` ran');
+}
+
+function handleDeleteItemClicked() {
+    // this function will be responsible for when users want to delete a shopping list
+    // item
+    console.log('`handleDeleteItemClicked` ran');
+}
+
+function handleShoppingList() {
+    // render the shopping list
+    // handle when a new item is submitted
+    // handle when an item is deleted
+    // handle when an item is checked/unchecked
+}
