f3d928495b30c7bcf054878646441923e42cbf0e
L Barbee <bluelb84@gmail.com>
2017-08-06 20:14:23 -0400

    hardcoding the shopping list item string and appended as html into DOM

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+// STORE object where data is stored about the shopping list
+const STORE = [
+    {name: "apples", checked: false},
+    {name: "oranges", checked: false},
+    {name: "milk", checked: true},
+    {name: "bread", checked: false}
+];
+
 function renderShoppingList() {
     // this function will be responsible for rendering the shopping lise in
     // the DOM
     console.log('`renderShoppingList` ran');
+    const shoppingListItemsString = '<li>hello world</li>';
+    // insert that HTML into the DOM
+    $('.js-shopping-list').html(shoppingListItemsString);
 }
+
+
 
 function handleNewItemSubmit() {
     // this function will be responsible for when users add a new shopping list item
