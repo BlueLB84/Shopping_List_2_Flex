b99def66e504d3daa6c0764f05b0fe68a10ef2b3
L Barbee <bluelb84@gmail.com>
2017-08-08 09:15:20 -0400

    added check item function, update STORE with item checked or unchecked, renderApp

--- app.js
+++ app.js
@@ -58,10 +58,23 @@ function handleNewItemSubmit() {
     });
 }
 
+function toggleCheckedForListItem(itemIndex) {
+    console.log("Toggling checked property for item at index " + itemIndex);
+    STORE[itemIndex].checked = !STORE[itemIndex].checked;
+}
+
+function getItemIndexFromElement(item){
+    const itemIndexString = $(item).closest('.js-item-index-element').attr('data-item-index');
+    return parseInt(itemIndexString, 10);
+}
+
 function handleItemCheckClicked() {
-    // this function will be responsible for when users click the "check" button on
-    // a shopping list item.
-    console.log('`handleItemCheckClicked` ran');
+    $('.js-shopping-list').on('click', '.js-item-toggle', event => {
+        console.log('`handleItemCheckClicked` ran');
+        const itemIndex = getItemIndexFromElement(event.currentTarget);
+        toggleCheckedForListItem(itemIndex);
+        renderShoppingList();
+    });
 }
 
 function handleDeleteItemClicked() {
